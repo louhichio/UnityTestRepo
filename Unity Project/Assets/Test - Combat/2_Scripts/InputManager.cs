c621c1f88286cdd78a7e1923737c48d333ed54ae
louhichio <louhichio@hotmail.com>
2016-04-22 20:56:58 +0200

    InputManager
    _Tap Detection
    _Swipe Detection
    _Mobile and Editor Input

---
+++ Unity Project/Assets/Test - Combat/2_Scripts/InputManager.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : Singleton<InputManager> 
+{
+	#region Properties
+	[SerializeField]
+	private float minSwipeInputLength = 10;
+	private float maxTapTimer = 0.5f;
+
+	private Vector3 touchStartPosition;
+	private float touchStartTime;
+
+	private float screenWidth;
+	private bool canAnalyseTouch = false;
+	#endregion
+
+	#region Unity
+	void Start()
+	{
+		screenWidth = Screen.width / 100;
+	}
+
+	void Update () 
+	{
+		#if UNITY_EDITOR
+
+		EditorInputCheck();
+
+		#elif UNITY_ANDROID || UNITY_IOS
+
+		MobileInputCheck();
+
+		#endif
+	}
+	#endregion
+
+	#region Private
+	private void EditorInputCheck()
+	{
+		if (Input.GetKeyDown (KeyCode.Mouse0)) 
+		{
+			touchStartTime = Time.time;
+			touchStartPosition = Input.mousePosition;
+		}
+
+		if (Input.GetKeyUp (KeyCode.Mouse0)) 
+		{
+			float dist = Vector3.Distance (touchStartPosition, Input.mousePosition);
+
+			if (dist >= minSwipeInputLength * screenWidth) 
+			{
+				UIManager.Instance.ShowText ("Swipe Detected");
+			} 
+			else 
+			{
+				float timer = Time.time - touchStartTime;
+
+				if (timer <= maxTapTimer) 
+				{
+					if (Input.mousePosition.x < Screen.width / 2) 
+					{
+						UIManager.Instance.ShowText ("Left Tap");
+					} 
+					else if (Input.mousePosition.x > Screen.width / 2) 
+					{
+						UIManager.Instance.ShowText ("Right Tap");
+					}
+				}
+			}
+		}
+	}
+
+	private void MobileInputCheck()
+	{
+		if (Input.touches.Length == 1)
+		{
+			Touch touch = Input.touches[0];
+			switch (touch.phase)
+			{
+			case TouchPhase.Began :		
+				canAnalyseTouch = true;
+				touchStartTime = Time.time;
+				touchStartPosition = touch.position;
+				break;
+
+			case TouchPhase.Canceled :	
+				canAnalyseTouch = false;
+				break;
+
+			case TouchPhase.Ended :		
+				if(canAnalyseTouch)
+				{
+					float dist = Vector3.Distance (touchStartPosition, touch.position);
+
+					if (dist >= minSwipeInputLength * screenWidth) 
+					{
+						UIManager.Instance.ShowText ("Swipe Detected");
+					} 
+					else 
+					{
+						float timer = Time.time - touchStartTime;
+
+						if (timer <= maxTapTimer) 
+						{
+							if (touch.position.x < Screen.width / 2) 
+							{
+								UIManager.Instance.ShowText ("Left Tap");
+							} 
+							else if (touch.position.x > Screen.width / 2) 
+							{
+								UIManager.Instance.ShowText ("Right Tap");
+							}
+						}
+					}
+				}
+				break;
+			}			
+		}
+		else if(canAnalyseTouch)	
+			canAnalyseTouch = false;
+	}
+	#endregion
+}
---
+++ Unity Project/Assets/Test - Combat/2_Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : Singleton<UIManager> 
+{
+	[SerializeField]
+	private Text text;
+
+	#region Unity
+	void Start () 
+	{
+	
+	}
+
+	void Update () {
+	
+	}
+	#endregion
+
+	#region Public
+	public void ShowText(string value)
+	{
+		text.text = value;
+	}
+	#endregion
+}
